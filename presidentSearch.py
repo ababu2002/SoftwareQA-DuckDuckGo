8dcd3eede23e296fa660a053bd5819c04e2b13eb
ababu2002 <ababu@my.waketech.edu>
2021-10-24 19:23:44 -0400

    Working presidentSearch

--- presidentSearch.py
+++ presidentSearch.py
@@ -2,9 +2,58 @@ import requests
 
 url_ddg = "https://api.duckduckgo.com"
 
+PresidentNames = ["Adams"
+,"Adams"
+,"Arthur"
+,"Biden"
+,"Buchanan"
+,"Bush"
+,"Bush"
+,"Carter"
+,"Cleveland"
+,"Clinton"
+,"Coolidge"
+,"Eisenhower"
+,"Fillmore"
+,"Ford"
+,"Garfield"
+,"Grant"
+,"Harding"
+,"Harrison"
+,"Harrison"
+,"Hayes"
+,"Hoover"
+,"Jackson"
+,"Jefferson"
+,"Johnson"
+,"Johnson"
+,"Kennedy"
+,"Lincoln"
+,"Madison"
+,"McKinley"
+,"Monroe"
+,"Nixon"
+,"Obama"
+,"Pierce"
+,"Polk"
+,"Reagan"
+,"Roosevelt"
+,"Roosevelt"
+,"Taft"
+,"Taylor"
+,"Truman"
+,"Trump"
+,"Tyler"
+,"Van Buren"
+,"Washington"
+,"Wilson"]
 
 def test_ddg0():
     resp = requests.get(url_ddg + "/?q=presidents of the united states&format=json")
     rsp_data = resp.json()
-    print(rsp_data)
-    assert "DuckDuckGo" in rsp_data["Heading"]
+    print(rsp_data["RelatedTopics"])
+    for topic in rsp_data["RelatedTopics"]:
+        for president in PresidentNames:
+            if president in topic["Result"]:
+                PresidentNames.remove(president)
+    assert PresidentNames == []
