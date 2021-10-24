774ccc33e30b6d372104679f92e3a91048a484d0
ababu2002 <ababu@my.waketech.edu>
2021-10-24 18:22:59 -0400

    testing president

--- presidentSearch.py
+++ presidentSearch.py
@@ -2,6 +2,7 @@ import requests
 
 url_ddg = "https://api.duckduckgo.com"
 def test_ddg0():
-    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
+    resp = requests.get(url_ddg + "/?q=presidents of the united states&format=json")
     rsp_data = resp.json()
+    print(rsp_data)
     assert "DuckDuckGo" in rsp_data["Heading"]
