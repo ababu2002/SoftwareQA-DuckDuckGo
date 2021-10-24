c8ac26af12948d83df01ca94d22a888b82aabfed
ababu2002 <ababu@my.waketech.edu>
2021-10-24 19:02:30 -0400

    edited presidentSearch

--- presidentSearch.py
+++ presidentSearch.py
@@ -1,6 +1,8 @@
 import requests
 
 url_ddg = "https://api.duckduckgo.com"
+
+
 def test_ddg0():
     resp = requests.get(url_ddg + "/?q=presidents of the united states&format=json")
     rsp_data = resp.json()
